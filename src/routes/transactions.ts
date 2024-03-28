e26b0ae7decdd21d44818ea18d92ea111fd7a863
Daniel Lucas <danielluccas000@gmail.com>
2024-03-28 00:21:54 -0300

    feat: create route summary

--- src/routes/transactions.ts
+++ src/routes/transactions.ts
@@ -22,6 +22,14 @@ export async function transactionsRoutes(app: FastifyInstance) {
     return { transaction };
   });
 
+  app.get('/summary', async () => {
+    const summary = await knex('transactions')
+      .sum('amount', { as: 'amount' })
+      .first();
+
+    return { summary };
+  });
+
   app.post('/', async (request, reply) => {
     const createTransactionBodySchema = z.object({
       title: z.string(),
