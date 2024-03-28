9ff406e5dd75fce95e2d2b39f0148f470421223d
Daniel Lucas <danielluccas000@gmail.com>
2024-03-28 00:15:56 -0300

    first commit

---
+++ src/routes/transactions.ts
@@ -0,0 +1,44 @@
+import { FastifyInstance } from 'fastify';
+import { z } from 'zod';
+import { knex } from '../database';
+import crypto from 'node:crypto';
+
+export async function transactionsRoutes(app: FastifyInstance) {
+  app.get('/', async () => {
+    const transactions = await knex('transactions').select('*');
+
+    return { transactions };
+  });
+
+  app.get('/:id', async (request) => {
+    const getTransactionParamsSchema = z.object({
+      id: z.string().uuid(),
+    });
+
+    const { id } = getTransactionParamsSchema.parse(request.params);
+
+    const transaction = await knex('transactions').where('id', id).first();
+
+    return { transaction };
+  });
+
+  app.post('/', async (request, reply) => {
+    const createTransactionBodySchema = z.object({
+      title: z.string(),
+      amount: z.number(),
+      type: z.enum(['credit', 'debit']),
+    });
+
+    const { title, amount, type } = createTransactionBodySchema.parse(
+      request.body
+    );
+
+    await knex('transactions').insert({
+      id: crypto.randomUUID(),
+      title,
+      amount: type === 'credit' ? amount : amount * -1,
+    });
+
+    return reply.status(201).send();
+  });
+}
